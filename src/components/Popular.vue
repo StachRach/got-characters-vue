e75db0930d896e662ea5bca72108908b2ee3f3b9
StachRach <srachwal2000@gmail.com>
2023-09-25 13:57:03 +0200

    Refactor

--- src/components/Popular.vue
+++ src/components/Popular.vue
@@ -2,32 +2,37 @@
 import PopularItem from "./PopularItem.vue";
 
 import {Person} from "../interfaces/Person.ts";
+import {onBeforeUpdate} from "vue";
 
 const props = defineProps<{
 	characters: Person[];
 }>();
 
-const king = props.characters
-	.filter((c: Person) => c.title.toLowerCase().includes("king"))
-	.splice(0, 1);
-const queen = props.characters
-	.filter((c: Person) => c.title.toLowerCase().includes("queen"))
-	.splice(1, 1);
-const royalties = [...king, ...queen]
+let king;
+let queen;
+let royalties: Person[] = [];
 
+onBeforeUpdate(() => {
+	king = props.characters
+		.filter((c: Person) => c.title.toLowerCase().includes("king"))
+		.splice(0, 1);
+	queen = props.characters
+		.filter((c: Person) => c.title.toLowerCase().includes("queen"))
+		.splice(1, 1);
+	royalties = [...king, ...queen]
+})
 </script>
 
 <template>
   <div class="box">
-    <div v-for="c in royalties">
-      <PopularItem
-	      class="component"
-	      :image-url=c.imageUrl
-	      :name=c.fullName
-	      :title=c.title
-	      :house=c.family
-      />
-    </div>
+	  <PopularItem
+		  v-for="c in royalties"
+		  class="component"
+		  :image-url=c.imageUrl
+		  :name=c.fullName
+		  :title=c.title
+		  :house=c.family
+	  />
   </div>
 </template>
 
